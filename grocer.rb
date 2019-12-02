4a15c20cccab3452aaf9323c9ee727632753cb97
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-02 19:36:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ end
   #Create a first order method once we finish and redo the apply_coupons. 
   
 def first_order(coupon) #we have to pass an arg and then take the item and round it out 
-  rounding = (coupon[:cost].to_f * 1.0 / coupon[:num].round(2))
+  rounding = (coupon[:cost].to_f * 1.0 / coupon[:num].round(2)
   { :item => "#{coupon[:item]} W/COUPON",
   :price => rounding,
   :count => coupon[:num]
