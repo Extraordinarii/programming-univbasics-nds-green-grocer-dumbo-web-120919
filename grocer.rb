d6094d03b77fa26053dd9965099a8489c7b5baed
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-02 19:29:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,9 +37,15 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  
+  #apply first method 
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < coupons.length
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  
 end
 
 def apply_clearance(cart)
